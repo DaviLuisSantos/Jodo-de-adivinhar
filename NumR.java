7a90219721268b417447bbfb34544d240a8d4983
Davizaun <costasantos.daviluis@gmail.com>
2023-01-19 11:04:37 -0300

    Version 1.0

--- NumR.java
+++ NumR.java
@@ -8,18 +8,41 @@ class Programa {
 
 	public static void AdvinhaNum(){
 		Scanner in = new Scanner(System.in);
-		System.out.println("Digite um número");
-        int s = Integer.parseInt(in.nextLine());
-        System.out.println("O número digitado é: "+s);
+		System.out.println("Digite o limite para o número que será gerado: ");
+		int limite=Integer.parseInt(in.nextLine());
 		Random numero = new Random();
-		int limite=3;
 		int num = numero.nextInt(limite);
-		if(num==s){
-			System.out.println("Os números são iguais!!");
+		System.out.println("O número foi gerado! ");
+		System.out.println("Agora tente advinhar o número que foi gerado:");
+        int s = Integer.parseInt(in.nextLine());
+        boolean repete=true;
+
+        while(repete){
+        	MaiorMenor(s,num);
+        	System.out.println("Tente Novamente");
+            s = Integer.parseInt(in.nextLine());
+        	repete=Teste(s,num);
+        }
+	}
+	public static void MaiorMenor(int numAdv, int numGer){
+		if(numAdv>numGer){
+			System.out.println("O número digitado é maior! Tente novamente");
+
 		}
-		else if(num!=s){
-			System.out.println("Os números são diferentes!!");
+		else if(numAdv<numGer){
+			System.out.println("O número digitado é menor! Tente novamente");
 		}
-		System.out.println("Número:"+ num);
+
+	} 
+	public static boolean Teste(int numAdv,int numGer){
+		boolean res=true;
+		if(numAdv==numGer){
+			System.out.println("Você acertou!!");
+			res=false;
+		}
+		else if(numAdv!=numGer){
+			res=true;
+		}
+		return res;
 	}
 }
